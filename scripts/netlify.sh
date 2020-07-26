11764f0e0b59db3a225407d760cfb5ca4e5e2ff7
Restyled.io <commits@restyled.io>
2020-07-26 02:08:53 +0000

    Restyled by shellharden

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -8,8 +8,8 @@ CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
 
 #### Build website
 # Provide the URL for this deployment to Mkdocs
-echo "${DEPLOY_PRIME_URL}" > "${CURRENT_DIR}/../CNAME"
-sed -i "s#site_url:.*#site_url: ${DEPLOY_PRIME_URL}#" "${CURRENT_DIR}/../mkdocs.yml"
+echo "$DEPLOY_PRIME_URL" > "$CURRENT_DIR/../CNAME"
+sed -i "s#site_url:.*#site_url: $DEPLOY_PRIME_URL#" "$CURRENT_DIR/../mkdocs.yml"
 
 # Build
 mkdocs build
